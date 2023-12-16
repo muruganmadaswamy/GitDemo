403acbeff09c3446dadff850aea90450fbc47e3a
muruganmadawamy <murugan.madaswamy@yahoo.com>
2023-12-16 23:23:42 +0530

    American guy commit

--- src/test/java/rahulshettyacademy/tests/ErrorValidationsTest.java
+++ src/test/java/rahulshettyacademy/tests/ErrorValidationsTest.java
@@ -26,6 +26,10 @@ public class ErrorValidationsTest extends BaseTest{
 		System.out.println("postJira");
 		System.out.println("postJira2");
 		System.out.println("postJira3");
+		System.out.println("postJira4");
+		System.out.println("postJira5");
+		System.out.println("postJira6");
+		System.out.println("postJira7");
 		
 	}
 	@Test
